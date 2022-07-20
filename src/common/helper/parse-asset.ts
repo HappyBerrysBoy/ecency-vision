1da82d16ebf4e4493967de0642ac8952ca67ca2f
Suncheol Kwon <elytselyts@gmail.com>
2022-07-20 17:11:30 +0900

    hive to steem 변화중

--- src/common/helper/parse-asset.ts
+++ src/common/helper/parse-asset.ts
@@ -1,33 +1,35 @@
-import { SMTAsset } from '@hiveio/dhive';
+import { SMTAsset } from "@hiveio/dhive";
 
 export enum Symbol {
-    HIVE = 'HIVE',
-    HBD = 'HBD',
-    VESTS = 'VESTS',
+  STEEM = "STEEM",
+  SBD = "SBD",
+  VESTS = "VESTS",
 }
 
 export enum NaiMap {
-    '@@000000021' = 'HIVE',
-    '@@000000013' = 'HBD',
-    '@@000000037' = 'VESTS',
+  "@@000000021" = "STEEM",
+  "@@000000013" = "SBD",
+  "@@000000037" = "VESTS",
 }
 
 export interface Asset {
-    amount: number,
-    symbol: Symbol
+  amount: number;
+  symbol: Symbol;
 }
 
 export default (sval: string | SMTAsset): Asset => {
-    if (typeof sval === 'string') {
-        const sp = sval.split(' ');
-        return {
-            amount: parseFloat(sp[0]),
-            symbol: Symbol[sp[1]]
-        }    
-    } else {
-        return {
-            amount: parseFloat(sval.amount.toString()) / Math.pow(10, sval.precision),
-            symbol: NaiMap[sval.nai]
-        }   
-    }
-}
+  if (typeof sval === "string") {
+    const sp = sval.split(" ");
+    return {
+      amount: parseFloat(sp[0]),
+      symbol: Symbol[sp[1]],
+    };
+  } else {
+    return {
+      amount: sval
+        ? parseFloat(sval.amount.toString()) / Math.pow(10, sval.precision)
+        : 0,
+      symbol: sval ? NaiMap[sval.nai] : null,
+    };
+  }
+};
--- src/common/helper/parse-date.ts
+++ src/common/helper/parse-date.ts
@@ -1,35 +1,65 @@
 import moment from "moment";
 
 export const dateToRelative = (d: string): string => {
-  const isTimeZoned = (d.indexOf('.')!==-1||d.indexOf('+')!==-1) ? d :`${d}.000Z`;
+  const isTimeZoned =
+    d.indexOf(".") !== -1 || d.indexOf("+") !== -1 ? d : `${d}.000Z`;
   const dm = moment(new Date(isTimeZoned));
   const dd = dm.fromNow(true);
-  return dd.replace('a few seconds','~1s').replace(' seconds','s').replace(' minutes','m').replace('a minute','1m').replace(' hours','h').replace('an hour','1h').replace(' days','d').replace('a day','1d').replace(' months','M').replace('a month','1M').replace(' years','y').replace('a year','1y');
-}
+  return dd
+    .replace("a few seconds", "~1s")
+    .replace(" seconds", "s")
+    .replace(" minutes", "m")
+    .replace("a minute", "1m")
+    .replace(" hours", "h")
+    .replace("an hour", "1h")
+    .replace(" days", "d")
+    .replace("a day", "1d")
+    .replace(" months", "M")
+    .replace("a month", "1M")
+    .replace(" years", "y")
+    .replace("a year", "1y");
+};
 
 export const dateToFullRelative = (d: string): string => {
-  const isTimeZoned = (d.indexOf('.')!==-1||d.indexOf('+')!==-1) ? d :`${d}.000Z`;
-  const dm = moment(new Date(isTimeZoned))
-  return dm.fromNow();
-}
+  console.log("dateToFullRelative:", d);
+  try {
+    const isTimeZoned =
+      d.indexOf(".") !== -1 || d.indexOf("+") !== -1 ? d : `${d}.000Z`;
+    const dm = moment(new Date(isTimeZoned));
+    return dm.fromNow();
+  } catch (err) {
+    return "";
+  }
+};
 
 export const dateToFormatted = (d: string, format: string = "LLLL"): string => {
-  const isTimeZoned = (d.indexOf('.')!==-1||d.indexOf('+')!==-1) ? d :`${d}.000Z`;
-  const dm = moment(new Date(isTimeZoned));
-  return dm.format(format);
-}
+  try {
+    const isTimeZoned =
+      d.indexOf(".") !== -1 || d.indexOf("+") !== -1 ? d : `${d}.000Z`;
+    const dm = moment(new Date(isTimeZoned));
+    return dm.format(format);
+  } catch (err) {
+    return "";
+  }
+};
 
 export const dayDiff = (d: string) => {
-  const isTimeZoned = (d.indexOf('.')!==-1||d.indexOf('+')!==-1) ? d :`${d}.000Z`;
-  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-  const a = new Date(isTimeZoned);
-  const b = new Date();
+  console.log("dayDiff:", d);
+  try {
+    const isTimeZoned =
+      d.indexOf(".") !== -1 || d.indexOf("+") !== -1 ? d : `${d}.000Z`;
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+    const a = new Date(isTimeZoned);
+    const b = new Date();
 
-  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
 
-  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
-}
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+  } catch (err) {
+    return "";
+  }
+};
 
 const parseDate = (d: string): Date => new Date(`${d}.000Z`);
 
